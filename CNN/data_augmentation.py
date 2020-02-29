6ab262244e0c144fd5929c4344376cb42625bafb
Saksham Kashyap <47538539+kashyap99saksham@users.noreply.github.com>
2020-02-29 19:11:49 +0530

    Update data_augmentation.py

--- CNN/data_augmentation.py
+++ CNN/data_augmentation.py
@@ -1,5 +1,6 @@
-//This is not executed code... just to understand .. how it makes many similar images from single image
-
+# This is not executed code... just to understand .. how it makes many similar images from single image
+# Visit for any help : 
+# https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 datagen = ImageDataGenerator(
